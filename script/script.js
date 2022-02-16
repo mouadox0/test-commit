dfb2113ae67afe710315f7500eb8815d2216ea71
mouadox0 <mouadboustta185@gmail.com>
2022-02-16 14:00:00 +0100

    commit in 2022

--- script/script.js
+++ script/script.js
@@ -57,7 +57,7 @@ function initTyping() {
         
         wpmTag.innerText = wpm;
         mistakeTag.innerText = mistakes;
-        cpmTag.innerText = charIndex - mistakes; // Ndir lkhatr l notre col v2222
+        cpmTag.innerText = charIndex - mistakes; // Ndir lkhatr l notre col v2222kdhajkdhhaaudhadhjahdjadjhh
     } else {
         clearInterval(timer);
         inpField.value = "";
